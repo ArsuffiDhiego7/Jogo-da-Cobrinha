99c82d818968e29534515bf1c4c03d7e2d4990e7
Dhiego Arsuffi <Dhiego Arsuffi@SIDNEI>
2023-06-13 13:20:19 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,105 @@
+//Dimensão do Tabuleiro
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+//Cobra head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+//velocidade
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
+//Comida
+var foodX;
+var foodY;
+
+var gameOver = false;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d");
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    // update();
+    setInterval(update, 1000/10); //100 milisegundos
+}
+
+function update() {
+    if (gameOver) {
+        return;
+    }
+    //Desenho do tabuleiro
+    context.fillStyle="black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    //Desenho da Comida
+    context.fillStyle="red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY])
+        placeFood();
+    }
+
+    for (let i = snakeBody.length-1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i-1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+    //Desenho da Cobrinha
+    context.fillStyle="lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //Condiçoes para o fim de jogo
+    if(snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) { //colisão do tabuleiro
+        gameOver = true;
+        alert("Game Over"); //Alerta para o fim de jogo
+    }
+    for (let i = 0; i < snakeBody.length; i++){
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) //colisão da própria cobrinha
+        gameOver = true;
+        alert("Game Over"); //Alerta para o fim de jogo
+    }        
+}
+
+
+//Funções da direção da Cobrinha
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1){
+        velocityX = 0;
+        velocityY = -1;
+    }
+    if (e.code == "ArrowDown" && velocityY != -1){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    if (e.code == "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    if (e.code == "ArrowRight" && velocityX != -1){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+//Movimento da comida
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
